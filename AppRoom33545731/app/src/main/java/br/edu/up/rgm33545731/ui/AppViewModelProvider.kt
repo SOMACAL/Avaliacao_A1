131d32b6b59a583f15a58cf4f1ca2b9735cf82a6
SOMACAL <crdacostacaio@gmail.com>
2024-11-26 18:10:30 -0300

    adicionado as funcionalidades do app

---
+++ AppRoom33545731/app/src/main/java/br/edu/up/rgm33545731/ui/AppViewModelProvider.kt
@@ -0,0 +1,55 @@
+package br.edu.up.rgm33545731.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import br.edu.up.rgm33545731.InventoryApplication
+import br.edu.up.rgm33545731.ui.home.HomeViewModel
+import br.edu.up.rgm33545731.ui.item.ItemDetailsViewModel
+import br.edu.up.rgm33545731.ui.item.ItemEditViewModel
+import br.edu.up.rgm33545731.ui.item.ItemEntryViewModel
+
+/**
+ * Provides Factory to create instance of ViewModel for the entire Inventory app
+ */
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        // Initializer for ItemEditViewModel
+        initializer {
+            ItemEditViewModel(
+                this.createSavedStateHandle(),
+                inventoryApplication().container.itemsRepository
+            )
+        }
+        // Initializer for ItemEntryViewModel
+        initializer {
+            ItemEntryViewModel(inventoryApplication().container.itemsRepository)
+        }
+
+        // Initializer for ItemDetailsViewModel
+        initializer {
+            ItemDetailsViewModel(
+                this.createSavedStateHandle(),
+                inventoryApplication().container.itemsRepository
+            )
+        }
+
+        // Initializer for HomeViewModel
+        initializer {
+            HomeViewModel(inventoryApplication().container.itemsRepository)
+        }
+    }
+}
+
+/**
+ * Extension function to queries for [Application] object and returns an instance of
+ * [InventoryApplication].
+ */
+fun CreationExtras.inventoryApplication(): InventoryApplication {
+    val application = this[AndroidViewModelFactory.APPLICATION_KEY]
+    return application as? InventoryApplication
+        ?: throw IllegalStateException("Application must be an instance of InventoryApplication")
+}
