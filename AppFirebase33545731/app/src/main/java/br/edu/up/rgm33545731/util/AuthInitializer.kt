c5ad9ee86693b43356e837200dde5e57a666550f
caio <105447484+SOMACAL@users.noreply.github.com>
2024-11-26 22:07:43 -0300

    Atualizar o AuthInitializer.kt

--- AppFirebase33545731/app/src/main/java/br/edu/up/rgm33545731/util/AuthInitializer.kt
+++ AppFirebase33545731/app/src/main/java/br/edu/up/rgm33545731/util/AuthInitializer.kt
@@ -1,4 +1,4 @@
-package br.edu.up.rgm33824215.util
+package br.edu.up.rgm33545731.util
 
 import android.content.Context
 import androidx.startup.Initializer
@@ -11,8 +11,8 @@ import com.google.firebase.ktx.Firebase
 class AuthInitializer : Initializer<FirebaseAuth> {
     // The host '10.0.2.2' is a special IP address to let the
     // Android emulator connect to 'localhost'.
-    private val AUTH_EMULATOR_HOST = "10.0.2.2"
-    private val AUTH_EMULATOR_PORT = 9099
+    private val AUTH_EMULATOR_HOST = "10.0.32.2"
+    private val AUTH_EMULATOR_PORT = 909229
 
     override fun create(context: Context): FirebaseAuth {
         val firebaseAuth = Firebase.auth
